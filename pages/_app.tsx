52363d3cc1e47060efd59984cf9c34eae7a6ea09
Mz <mahdi.zangeneh@whipmobility.com>
2023-06-04 15:53:16 +0800

    commenting nProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
-import Router from "next/router";
-import nProgress from "nprogress";
+// import Router from "next/router";
+// import nProgress from "nprogress";
 
 interface Props {
   session?: Session | null;
 }
 
-nProgress.configure({ showSpinner: false });
-Router.events.on("routeChangeStart", () => nProgress.start());
-Router.events.on("routeChangeComplete", () => nProgress.done());
-Router.events.on("routeChangeError", () => nProgress.done());
+// nProgress.configure({ showSpinner: false });
+// Router.events.on("routeChangeStart", () => nProgress.start());
+// Router.events.on("routeChangeComplete", () => nProgress.done());
+// Router.events.on("routeChangeError", () => nProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps<Props>) {
   return (
